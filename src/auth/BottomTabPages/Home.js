ac86d2067e034064010204764e3757e2a0a87a99
SiltuDevelops <machinevaneray@Laptop-van-Siltu.local>
2023-11-28 08:49:19 +0100

    Tue Nov 28 08:49:19 CET 2023

--- src/auth/BottomTabPages/Home.js
+++ src/auth/BottomTabPages/Home.js
@@ -1,11 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import React from 'react'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>Home</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
